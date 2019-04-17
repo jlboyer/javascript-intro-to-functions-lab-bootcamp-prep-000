d6dc905a93a41fe949cd31ac9738c9c2ced41989
mathematical-algorithm-3478 <jlboyer1@gmail.com>
2019-04-17 22:35:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function logWhisper(input){
 
 
 function sayHiToGrandma(input){
-  let output = (input === input.toLowerCase()) ? "I can't hear you!" : (input === input.toUpperCase()) ? "YES INDEED!" : (input === "I love you, Grandma") ? "I love you, too." : "wtf"
+  let output = (input === input.toLowerCase()) ? "I can't hear you!" : (input === input.toUpperCase()) ? "YES INDEED!" : (input === "I love you, Grandma") ? "I love you, too."
   console.log(output)
   return(output)
 };
